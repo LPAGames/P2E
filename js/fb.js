248cb7a7acdce2c3a41050cb03b4459bc94268a6
LPAGames <47326261+LPAGames@users.noreply.github.com>
2023-12-28 20:27:04 -0300

    testing login

--- js/fb.js
+++ js/fb.js
@@ -1,20 +1,12 @@
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+// Initialize the FirebaseUI Widget using Firebase.
+var ui = new firebaseui.auth.AuthUI(firebase.auth());
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyDVevm68YOQAd01bMTyaswAb9j8-TJOvvM",
-  authDomain: "p2earn-f36ae.firebaseapp.com",
-  projectId: "p2earn-f36ae",
-  storageBucket: "p2earn-f36ae.appspot.com",
-  messagingSenderId: "596533943453",
-  appId: "1:596533943453:web:425590cc182078b1754874",
-  measurementId: "G-2S1Q79MMN5"
-};
-
-// Initialize Firebase
-const app = firebase.initializeApp(firebaseConfig);
-// const analytics = firebase.getAnalytics(app);
-// const auth = firebase.getAuth(app);
-// const firestore = getFirestore(app);
+ui.start('#firebaseui-auth-container', {
+  signInOptions: [
+    {
+      provider: firebase.auth.EmailAuthProvider.PROVIDER_ID,
+      signInMethod: firebase.auth.EmailAuthProvider.EMAIL_LINK_SIGN_IN_METHOD
+    }
+  ],
+  // Other config options...
+});

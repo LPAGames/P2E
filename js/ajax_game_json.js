dab677d8c6d0e489ce8d30cd57d907e0e59714a9
LPAGames <47326261+LPAGames@users.noreply.github.com>
2023-12-28 15:23:20 -0300

    testing json

--- js/ajax_game_json.js
+++ js/ajax_game_json.js
@@ -10,23 +10,27 @@ $(document).ready(function() {
                 success: function(data) {
                   $.each(data, function(index, item) {
           
-                      var newParsedHtml = parsedHTML.clone();
-                      var youtubeFrame = $('<iframe class="w-100 h-100 m-0 p-0" src="https://www.youtube.com/embed/' +
-                          item.youtube_id + '?autoplay=0&mute=1&controls=0"></iframe>');
-                      $(newParsedHtml).find('#card-video').append(youtubeFrame);
-                      $(newParsedHtml).find('#card-title').text(item.title);
-                      
-                      cardBodyBack.append(cardTitle);
-                      if (item.image) {
-                          var cardImg = $('<div class="card-img align-content-center m-0 p-1" style="height: 60%;">' +
-                              '<img src="' + item.image + '" class="w-100 h-100" alt="Card image"></div>');
-                          cardBodyBack.append(cardImg);
-                          //card_back
-                      }
-                      
-                      // Example: Append parsed content to a container
-                      $('#game_cards').append(parsedHTML);
-                      });
+                        var newParsedHtml = parsedHTML;
+
+                        //front-card
+                        var youtubeFrame = $('<iframe class="w-100 h-100 m-0 p-0" src="https://www.youtube.com/embed/' +
+                            item.youtube_id + '?autoplay=0&mute=1&controls=0"></iframe>');
+                        $(newParsedHtml).find('#card-video').append(youtubeFrame);
+                        $(newParsedHtml).find('#card-title').text(item.title);
+                        
+                        $(newParsedHtml).find('#card-price').append('<b>Price $<br />' + item.price +'</b>');
+                        $(newParsedHtml).find('#card-pool').append('<b>Pool $<br />' + item.pool +'</b>');
+                        
+                        //card_back
+                        $(newParsedHtml).find('#card-back-title').text(item.title);
+                        if (item.image) {
+                            var cardImg = $('<img src="' + item.image + '" class="w-100 h-100" alt="Card image"></div>');
+                            cardBodyBack.append(cardImg);
+                        }
+                        
+                        // Example: Append parsed content to a container
+                        $('#game_cards').append(newParsedHtml);
+                    });
                   
                 }
             });

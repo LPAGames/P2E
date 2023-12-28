726361fd056317774388c2cb489dd9ed394c3631
LPAGames <47326261+LPAGames@users.noreply.github.com>
2023-12-28 03:38:11 -0300

    testing json

--- js/ajax_game_json.js
+++ js/ajax_game_json.js
@@ -6,12 +6,11 @@ $(document).ready(function() {
         $.each(data, function(index, item) {
             var cardContainer = $('<div class="card-container border-2 shadow-lg mx-1 mb-2"></div>');
             var card = $('<div class="card card-flip w-100 h-100"></div>');
-            cardContainer.append(card);
-
+            
             var youtubeLink = $('<div class="card-img align-content-center w-100 m-0 p-0" ' +
                 'style="height: 60%;"></div>');
             var youtubeFrame = $('<iframe class="w-100 h-100 m-0 p-0" src="https://www.youtube.com/embed/' +
-                item.youtubeLink + '?autoplay=0&mute=1&controls=0"></iframe>');
+                item.youtube_id + '?autoplay=0&mute=1&controls=0"></iframe>');
             
             youtubeLink.append(youtubeFrame);
             
@@ -19,8 +18,8 @@ $(document).ready(function() {
 
             var cardPricePool = $('<div class="container d-flex align-items-center justify-content-around mb-1 ">' + 
                 '<p class="card-subtitle text-center text-warning rounded bg-dark border border-warning shadow py-1 px-2">' +
-              '<b>Price $<br />' + item.price + 
-              '</b></p><p class="card-subtitle text-center text-warning rounded bg-dark border border-warning shadow py-1 px-1">' +
+              '<b>Price $<br />' + item.price + '</b></p>' + 
+              '<p class="card-subtitle text-center text-warning rounded bg-dark border border-warning shadow py-1 px-1">' +
               '<b>Pool $<br />' + item.pool +'</b></p></div>');
             //card_front
             var cardBodyFront = $('<div class="front card-body"></div>');
@@ -39,7 +38,7 @@ $(document).ready(function() {
             cardBodyBack.append(cardTitle);
             card.append(cardBodyBack);
             cardContainer.append(card);
-    
+            
             $('#game_cards').append(cardContainer);
         });
       }

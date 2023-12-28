9fb1d55c0055307a30eef245ec133cf66f101af0
LPAGames <47326261+LPAGames@users.noreply.github.com>
2023-12-28 02:14:30 -0300

    testing json

--- js/p2e.js
+++ js/p2e.js
@@ -1,10 +1,11 @@
+
 $(document).ready(function() {
     var front = document.getElementsByClassName("front");
     var back = document.getElementsByClassName("back");
   
     var highest = 0;
     var absoluteSide = "";
-  
+    
     for (var i = 0; i < front.length; i++) {
       if (front[i].offsetHeight > back[i].offsetHeight) {
         if (front[i].offsetHeight > highest) {
@@ -19,4 +20,5 @@ $(document).ready(function() {
     $(".front").css("height", highest);
     $(".back").css("height", highest);
     $(absoluteSide).css("position", "absolute");
-  });  
+  });
+

0359152dddeb2e1af0d9711c2ed3a162ac332f51
LPAGames <47326261+LPAGames@users.noreply.github.com>
2023-12-28 16:28:40 -0300

    Signed-off-by: LPAGames <47326261+LPAGames@users.noreply.github.com>

--- js/ajax_game_json.js
+++ js/ajax_game_json.js
@@ -1,21 +1,19 @@
 $(document).ready(function() {
     $.ajax({
-        url: 'card.html',
-        success: function (data) { 
-            var parsedHTML = $.parseHTML(data);
-
-            $.ajax({
-                url: 'data/cards.json',
-                dataType: 'json',
-                success: function(data) {
-                  $.each(data, function(index, item) {
-          
+        url: 'data/cards.json',
+        dataType: 'json',
+        success: function(data) {
+          $.each(data, function(index, item) {
+                $.ajax({
+                    url: 'card.html',
+                    success: function (data) { 
+                        var parsedHTML = $.parseHTML(data);
                         var newParsedHtml = parsedHTML;
-                        
-                        var videoRemove = $(newParsedHtml).find('#parsed-video-rem');
-                        if(videoRemove)
-                            $(newParsedHtml).find('#parsed-video-rem').remove();
-                            
+                
+                        // var videoRemove = $(newParsedHtml).find('#parsed-video-rem');
+                        // if(videoRemove)
+                        //     $(newParsedHtml).find('#parsed-video-rem').remove();
+
                         //front-card
                         var youtubeFrame = $('<iframe id="parsed-video-rem" class="w-100 h-100 m-0 p-0" src="https://www.youtube.com/embed/' +
                             item.youtube_id + '?autoplay=0&mute=1&controls=0"></iframe>');
@@ -29,9 +27,9 @@ $(document).ready(function() {
                         
                         //card_back
                         $(newParsedHtml).find('#card-back-title').text(item.title);
-                        var imgRemove = $(newParsedHtml).find('#parsed-img-rem');
-                        if(imgRemove)
-                            $(newParsedHtml).find('#parsed-img-rem').remove();
+                        // var imgRemove = $(newParsedHtml).find('#parsed-img-rem');
+                        // if(imgRemove)
+                        //     $(newParsedHtml).find('#parsed-img-rem').remove();
                         if (item.image) {
                             var cardImg = $('<img id="parsed-img-rem" src="' + item.image + '" class="w-100 h-100" alt="Card image"></div>');
                             $(newParsedHtml).find('#card-back-img').append(cardImg);
@@ -39,14 +37,16 @@ $(document).ready(function() {
                         
                         // Example: Append parsed content to a container
                         $('#game_cards').append(newParsedHtml);
-                    });
-                  
-                }
+                                
+                    
+                                
+                    }
+                });
+                
             });
-
-            
         }
     });
+    
     
     
   });
